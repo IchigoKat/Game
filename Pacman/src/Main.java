ed60b43bf5d9d1978194ab17188bd38968594ef7
sheryl_chin <sheryl_chin@student.wayland.k12.ma.us>
2017-03-13 07:49:38 -0400

    made a main file

---
+++ Pacman/src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by sheryl_chin on 3/13/17.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("this is a test");
+        int x = 4;
+    }
+}
