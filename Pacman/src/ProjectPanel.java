d5529682bb1e3ca20ab731ccd7e524e482690dad
sunjae_lee <Sunjae_Lee@student.wayland.k12.ma.us>
2017-03-15 10:01:39 -0400

    yo

---
+++ Pacman/src/ProjectPanel.java
@@ -0,0 +1,86 @@
+/**
+ * Created by sunjae_lee on 3/15/17.
+ */
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class ProjectPanel extends JPanel {
+    public static final int FRAMEWIDTH = 1000, FRAMEHEIGHT = 600;
+    private Timer timer;
+    private boolean[] keys;
+
+    public ProjectPanel() {
+        keys = new boolean[512];
+
+        timer = new Timer(40, new ActionListener() {
+
+            public void actionPerformed(ActionEvent actionEvent) {
+
+                //move the frog
+                if (keys[KeyEvent.VK_W]) {
+
+
+                    keys[KeyEvent.VK_W] = false; //probably.
+                }
+                if (keys[KeyEvent.VK_A]) {
+
+                    keys[KeyEvent.VK_A] = false;
+                }
+                if (keys[KeyEvent.VK_D]) {
+
+                    keys[KeyEvent.VK_D] = false;
+                }
+                if (keys[KeyEvent.VK_S]) {
+
+
+                    keys[KeyEvent.VK_S] = false;
+                }
+            }
+
+            repaint();
+        });
+        timer.start();
+
+        addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent keyEvent) {
+
+            }
+
+            @Override
+            public void keyPressed(KeyEvent keyEvent) {
+                keys[keyEvent.getKeyCode()] = true;
+            }
+
+            @Override
+            public void keyReleased(KeyEvent keyEvent) {
+                keys[keyEvent.getKeyCode()] = false;
+            }
+        });
+
+    }
+    public static void main(String[] args) {
+        int w = 420;
+        int h = 550;
+        JFrame window = new JFrame("");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setBounds(500, 0, w, h);
+
+        ProjectPanel panel = new ProjectPanel();
+        panel.setSize(FRAMEWIDTH,FRAMEHEIGHT);
+        panel.setFocusable(true);
+        panel.grabFocus();
+
+
+        window.add(panel);
+        window.setVisible(true);
+        window.setResizable(false);
+        window.setSize(FRAMEWIDTH, FRAMEHEIGHT);
+
+    }
+}
