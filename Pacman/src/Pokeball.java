3ed734352eb73a0054cc78d86cab33cc9dbf21c3
sheryl_chin <sheryl_chin@student.wayland.k12.ma.us>
2017-04-03 08:49:11 -0400

    changed ash to olimer added pokeball to screen

---
+++ Pacman/src/Pokeball.java
@@ -0,0 +1,9 @@
+/**
+ * Created by sheryl_chin on 3/31/17.
+ */
+public class Pokeball extends Sprite{
+    public Pokeball(int x,int y){
+        super(x,y,EAST);
+        setPic("pokeball.png",NORTH);
+    }
+}
